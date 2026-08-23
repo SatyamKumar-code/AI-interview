2f51178a4ec157a8bde1afac30304c84156bbf0b
SatyamKumar-code <satyamkumarsing59@gmail.com>
2026-08-23 14:03:27 +0530

    feat: implement authentication context and hooks, add CORS support, and enhance login/register forms

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,39 +1,65 @@
-import React from 'react'
-import "../auth.form.scss"
-import { Link, useNavigate } from 'react-router'
+import React from 'react';
+import "../auth.form.scss";
+import { Link, useNavigate } from 'react-router';
+import { useAuth } from "../hooks/useAuth";
+import { useState } from 'react';
 
 const Login = () => {
-
+    const { loading, handleLogin } = useAuth();
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await handleLogin({ email, password });
+        navigate('/');
+    };
+
+    if (loading) {
+        return (
+            <main><h1>Loading....</h1></main>
+        );
     }
 
-  return (
-    <main>
-        <div className="form-container">
-            <h1>Login</h1>
+    return (
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
 
-            <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='Enter email address'
+                        />
+                    </div>
 
-                <div className="input-group">
-                    <label htmlFor='eamil'>Email</label>
-                    <input type='email' id='email' name='email' placeholder='Enter email address' />
-                </div>
+                    <div className="input-group">
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Enter password'
+                        />
+                    </div>
 
-                <div className="input-group">
-                    <label htmlFor='password'>password</label>
-                    <input type='password' id='password' name='password' placeholder='Enter password' />
-                </div>
+                    <button type='submit' className='button primary-button'>Login</button>
+                </form>
 
-                <button className='button primary-button'>Login</button>
-            </form>
+                <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
+            </div>
+        </main>
+    );
+};
 
-            <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
-        </div>
-    </main>
-  )
-}
-
-export default Login
+export default Login;
