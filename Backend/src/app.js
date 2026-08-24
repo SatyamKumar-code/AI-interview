9a75eb6013385389943cc8064c4dff05a205f683
SatyamKumar-code <satyamkumarsing59@gmail.com>
2026-08-24 23:07:04 +0530

    feat: add interview report generation feature with Zod validation and new routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const authRouter = require("./routes/auth.routes");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes")
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(cors({
 }))
 
 app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
 module.exports = app;
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,16 +1,52 @@
 const { GoogleGenAI } = require("@google/genai");
+const { z } = require("zod");
+const { zodToJsonSchema } = require("zod-to-json-schema");
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
 })
 
-async function invokGeminiAi() {
+const interviewReportSchema = z.object({
+    matchScore: z.number().describe("A score between 0 and 100 indicating how well teh candidate's profile matches the job description."),
+    technicalQuestions: z.array(z.object({
+        question: z.string().describe("The technical question can be asked in the interview."),
+        intention: z.string().describe("The intention of interviewer behind asking this question"),
+        answer: z.string().describe("How to answer this question, what points to cover, whata approach to take etc."),
+    })).describe("Technical questions that can be asked in the interview along with their intention and how to answer them."),
+    behavioralQuestions: z.array(z.object({
+        question: z.string().describe("The technical question can be asked in the interview."),
+        intention: z.string().describe("The intention of interviewer behind asking this question"),
+        answer: z.string().describe("How to answer this question, what points to cover, whata approach to take etc."),
+    })).describe("Behavioral questions that can be asked in the interview along with their intention and how to answer them."),
+    skillGaps: z.array(z.object({
+        skill: z.string().describe("The skill which that candidate is lacking."),
+        severity: z.enum(["low", "medium", "high"]).describe("The severity of this skill gap, i.e."),
+    })).describe("List of skill gaps in the candidate's frofile along with their severity."),
+    preparationPlan: z.array(z.object({
+        day: z.number().describe("The day number of the preparation plan, starting from 1"),
+        focus: z.string().describe("The main focus of this day in the preparation plan, e.g. data structures, system design, etc."),
+        tasks: z.array(z.string()).describe("List of tasks to be done on this day to follow the preparation plan."),
+    })).describe("A day-wise preparation plan for the candidate to follow in order to prepare for the interview effactively."),
+})
+
+
+async function generateInterviewReport({ resume, selfDescription, jobDescription }) {
+
+    const prompt = `Generate an interview report for the candidate with the following details:
+    Resume: ${resume}
+    Self Description: ${selfDescription}
+    Job Description: ${jobDescription}
+    `
     const response = await ai.models.generateContent({
-        model: "gemini-3.6-flash",
-        contents: "Hell gemini ! Explain what is Interview"
+        model: "gemini-3-flash-preview",
+        contents: prompt,
+        config: {
+            responseMimeType: "application/json",
+            responseSchema: zodToJsonSchema(interviewReportSchema)
+        }
     })
 
-    console.log(response.text)
+    return JSON.parse(response.text);
 }
 
-module.exports = invokGeminiAi
+module.exports = generateInterviewReport
